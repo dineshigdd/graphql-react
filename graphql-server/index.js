21aa187e92d3f55d606c2df961275785d952aacd
unknown <dineshigdd@gmail.com>
2023-05-16 03:32:53 -0700

    remove temp files

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -6,35 +6,45 @@ const typeDefs = `
 type Author {
   id: ID!
   name: String! # returns a String and non-nullable
-  books: [Book!]!# A list of Books 
+  books(limit: Int = 3 ): [Book!]!# A list of Books 
   age:Int
 }
 
 type Book {
   id: ID!
   title: String!
-  author: Author! # returns an Author
+  publicationDate: String
 }
 
 type Query {
   authors: [Author!]!
   author(id: ID!): Author!
-  books: [Book!]!
-  book(id: ID!): Book!
+}
+
+type Mutation {
+  createAuthor(name: String!, age: Int): Author!
+
+ #feel free to add resolvers for update and delete
+ # updateAuthor(id: ID!, name: String, age: Int): Author! 
+ # deleteAuthor(id: ID!): Author!
 }
 `;
 
 //sample dataset. We will fetch data from these array of objects
 const authors = [
-  { id: "1", name: "J.K. Rowling" , age:null},
-  { id: "2", name: "Stephen King",age : 75 },
-  { id: "3", name: "Haruki Murakami",age:74 },
+  { id: "A1", name: "J.K. Rowling" , age:null}, 
+  { id: "A2", name: "Stephen King",age : 75 },
+  { id: "A3", name: "Haruki Murakami",age:74 },
+  
 ];
 
 const books = [
-  { id: "1", title: "Harry Potter and the Philosopher's Stone", authorId: "1" },
-  { id: "2", title: "The Shining", authorId: "2" },
-  { id: "3", title: "1Q84", authorId: "3" },
+  { id: "B1", title: "Harry Potter and the Philosopher's Stone", authorId: "A1" },
+  { id: "B2", title: "The Shining", authorId: "A2" },
+  { id: "B3", title: "1Q84", authorId: "A3" },
+  { id: "B4", title: "The Stand", authorId: "A2" },
+  { id: "B5", title: "Norwegian Wood", authorId: "A3" },
+  { id: "B6", title: "The Green Mile", authorId: "A2" }
 ];
 
 
@@ -42,24 +52,34 @@ const books = [
 const resolvers = {
   // Define resolvers for queries
   Query: {
-    // Return all books
-    books: () => books,
-    // Find a book by ID and return it
-    book: (parent, args) => books.find(book => book.id === args.id),
-    // Return all authors
+    
     authors: () => authors,
     // Find an author by ID and return them
     author: (parent, args) => authors.find(author => author.id === args.id)
   },
   // Define resolvers for Author type fields
   Author: {
-    // Return all books written by the author
-    books: (parent) => books.filter(book => book.authorId === parent.id)
+  // Return all books written by the author 
+    books: (parent , args ) => {
+      console.log( parent )
+      return books.filter(book => book.authorId === parent.id).slice( 0, args.limit )
+    }
   },
-  // Define resolvers for Book type fields
-  Book: {
-    // Return the author of the book
-    author: (parent) => authors.find(author => author.id === parent.authorId)
+ 
+
+
+  Mutation: {
+    createAuthor: (parent, args) => {
+   
+      const newAuthor = {
+        id: "A"+  ( authors.length + 1 ),
+        name: args.name,
+        age: args.age,
+        books: []
+      };
+      authors.push(newAuthor);
+      return newAuthor;
+    }
   }
 };
 
