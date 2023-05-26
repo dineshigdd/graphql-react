e48bd27422021aef9cee3a123dda3d1098cfff00
unknown <dineshigdd@gmail.com>
2023-05-25 22:10:11 -0700

    change structure

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -2,6 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './graphql/typeDef.js';
 import { resolvers } from './graphql/resolvers.js';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 
@@ -14,6 +15,14 @@ const server = new ApolloServer({
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function.
 // start the server at port 4000 on localhost
+dotenv.config();
+const connection_url = process.env.URL;
+
+mongoose.connect( connection_url,{
+    useNewUrlParser: true,   //to avoid warinings
+     useUnifiedTopology: true
+}).then( ()=>console.log( "connected to DB"));
+
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
